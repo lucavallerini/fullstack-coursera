917f66b4fd66719aa19c16aceb7f96faa553eea9
Luca Vallerini <lucavall90@gmail.com>
2019-01-22 11:16:44 +0100

    Assignment completed

--- course1/practice/04/module3_1_assignment.rb
+++ course1/practice/04/module3_1_assignment.rb
@@ -1,5 +1,13 @@
 require 'httparty'
 
 class Recipe
+   include HTTParty
 
+   base_uri "http://food2fork.com/api"
+   default_params key: ENV["FOOD2FORK_KEY"]
+   format :json
+
+   def self.for(keyword)
+      get(base_uri + "/search", { query: default_params.merge({q: keyword}) })['recipes']
+   end
 end
