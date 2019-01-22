c9dadfb97df6abcfe0de0d02bea4d8e0c937484a
Luca Vallerini <lucavall90@gmail.com>
2019-01-22 15:05:38 +0100

    Assignment completed

---
+++ course1/assignment/assignment01-Recipe-Hunter/recipefinder/app/models/recipe.rb
@@ -0,0 +1,12 @@
+class Recipe
+   include HTTParty
+
+   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'food2fork.com'
+   base_uri "http://#{hostport}/api"
+   default_params key: ENV['FOOD2FORK_KEY']
+   format :json
+
+   def self.for(searchterm) 
+      get(base_uri + "/search", { query: default_params.merge({q: searchterm}) })['recipescd ']
+   end
+end
