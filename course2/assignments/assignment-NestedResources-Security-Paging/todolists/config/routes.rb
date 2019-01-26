fb3d85606a4aa83b1c4e36115512499f33edd106
Luca Vallerini <lucavall90@gmail.com>
2019-01-26 12:54:48 +0100

    Assignment completed

--- course2/assignments/assignment-NestedResources-Security-Paging/todolists/config/routes.rb
+++ course2/assignments/assignment-NestedResources-Security-Paging/todolists/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,6 +10,9 @@ Rails.application.routes.draw do
   resources :todo_lists do
     resources :todo_items
   end
+
+  get '/login' => "sessions#new", as: "login"
+  delete '/logout' => "sessions#destroy", as: "logout"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
